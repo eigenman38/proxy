e647ce6686e432e9886350f19f015b1eea35ee5d
jf <eigenman38@gmail.com>
2022-09-03 09:06:38 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proxy';
+  private url = 'http://localhost:4200/api/things';
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.get();
+  }
+
+  private get(): void {
+    this.http.get(this.url).subscribe(x => {
+      console.log(JSON.stringify(x));
+
+    });
+
+  }
 }
+
+
